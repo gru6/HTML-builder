e35444b429ea92869d8bc090001f263a1b1db7e8
gru6 <iliagrushevich@gmail.com>
2022-05-23 20:30:48 +0300

    feat: add 04 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,15 +8,13 @@ fs.readdir(directoryPath, { withFileTypes: true }, function (err, files) {
         return console.log('Unable to scan directory: ' + err);
     }
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
         if (file.isFile()) {
 
-            fs.stat(path.join(directoryPath, file.name /* ? */), (err, stats) => {
+            fs.stat(path.join(directoryPath, file.name), (err, stats) => {
                 if (err) {
                     console.error(err);
                 }
-                // we have access to the file stats in `stats`
-                const name = path.parse(file.name).name; /* ? */
+                const name = path.parse(file.name).name;
                 const type = path.extname(file.name);
                 const sise = stats.size;
                 console.log(`${name} - ${type.slice(1)} - ${sise / 1000}kb`)
@@ -24,5 +22,3 @@ fs.readdir(directoryPath, { withFileTypes: true }, function (err, files) {
         }
     });
 });
-
-/* а в Image.jpg не должен файл появиться? */
